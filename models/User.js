76e11bc3a49db5de7be9a6b0f5e22699940d236d
fremen432 <cbmiller4897@gmail.com>
2021-12-02 17:35:00 -0600

    git ignore

---
+++ models/User.js
@@ -0,0 +1,11 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            
+        }
+    }
+)
