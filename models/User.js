e107b8d14612b33500d9db2be1b473699942dad6
fremen432 <cbmiller4897@gmail.com>
2021-12-02 18:00:08 -0600

    User model start

--- models/User.js
+++ models/User.js
@@ -1,11 +1,37 @@
 const { Schema, model } = require('mongoose');
-const dateFormat = require('../utils/dateFormat');
 
 const UserSchema = new Schema(
     {
         username: {
             type: String,
             
+        },
+        email: {
+            type: String,
+
+        },
+        thoughts: {
+            // Array of _id values referencing the Thought model
+        },
+        friends: {
+            // Array of _id values referencing the Thought model
         }
-    }
-)
+    },
+    {
+        toJSON: {
+          virtuals: true,
+          getters: true
+        },
+        // prevents virtuals from creating duplicate of _id as `id`
+        id: false
+      }
+);
+
+// get the length of the user's friends array field on query.
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length
+});
+  
+const User = model('User', UserSchema);
+
+module.exports = User;
